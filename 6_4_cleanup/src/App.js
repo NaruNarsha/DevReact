202c4a84d00dc1a544538dbe5737abb77b2c523d
문경림 <naru@takeone.co.kr>
2025-11-17 09:42:54 +0900

    update 6.4 cleanup

--- 6_4_cleanup/src/App.js
+++ 6_4_cleanup/src/App.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react"
+
+
+function Hello()
+{
+  function byFn(){
+    console.log("bye :(");
+  }
+
+  function hiFn(){
+    console.log("created :)");
+
+    return byFn;
+  };
+
+  useEffect(hiFn, []);
+
+  return <h1>Hello</h1>
+}
 
 function App() {
-  return (
-    <div className="App">
 
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {/** 자바 스크립트를 쓸 때 중괄호를 쓰는것을 기억해야한다!!! */}
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
